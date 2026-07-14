f64085f6ab0d5a8567d3f58d92adf8001e37313a
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-07-14 21:52:20 +0530

    Files

--- Files/dashboard/app.py
+++ Files/dashboard/app.py
@@ -1,63 +1,3 @@
-# # Dashboard Part
-# import streamlit as st
-# import pandas as pd
-# import numpy as np
-# import joblib
-# import plotly.express as px
-# import plotly.graph_objects as go
-
-# st.set_page_config(
-#     page_title="AirPulse AI",
-#     page_icon="🌍",
-#     layout="wide"
-# )
-
-# st.title("🌍 AirPulse AI")
-
-# st.subheader(
-#     "Intelligent Global Air Quality Monitoring & Prediction System"
-# )
-
-
-# st.sidebar.title("Navigation")
-
-# page = st.sidebar.radio(
-#     "Go to",
-#     [
-#         "Home",
-#         "Dashboard",
-#         "AQI Prediction",
-#         "AQI Classification",
-#         "Model Performance",
-#         "About"
-#     ]
-# )
-
-
-# df = pd.read_csv("../data/processed_air_quality.csv")
-
-
-# if page == "Home":
-#     st.header("Welcome to AirPulse AI")
-#     st.write(
-#         """
-#         AirPulse AI is an intelligent air quality monitoring
-#         and prediction system developed using Machine Learning.
-
-#         This application provides:
-
-#         - AQI Prediction
-#         - AQI Classification
-#         - Interactive Visualizations
-#         - Air Quality Insights
-#         """
-#     )
-    
-
-# st.subheader("Dataset Preview")
-
-# st.dataframe(df.head())
-
 # ==========================================================
 # 🌍 AirPulse AI
 # Intelligent Global Air Quality Monitoring &
@@ -75,7 +15,6 @@ import joblib
 
 import plotly.express as px
 import plotly.graph_objects as go
-
 
 # ===========================
 # Page Configuration
@@ -272,13 +211,6 @@ if page == "🏠 Home":
     # =====================================================
     # Dataset Preview
     # =====================================================
-
-    # st.subheader("📋 Dataset Preview")
-
-    # st.dataframe(
-    #     df.head(10),
-    #     use_container_width=True
-    # )
 
     with st.expander("📋 Dataset Preview (Click to View Top 10 Records)"):
         st.write("### Top 10 Records")
