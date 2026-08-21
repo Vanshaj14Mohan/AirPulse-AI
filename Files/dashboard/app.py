98f9cd1c71bb4a94292822487e3eb181e02fc2e9
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-08-21 23:49:09 +0530

    Files

--- Files/dashboard/app.py
+++ Files/dashboard/app.py
@@ -466,10 +466,6 @@ elif page == "📊 Dashboard":
 
         st.info("🚀 Phase 6.2 - Part 1 Completed")
 
-# elif page == "🤖 AQI Prediction":
-#     st.title("🤖 AQI Prediction")
-#     st.info("Coming in Phase 6.3")
-
 elif page == "🤖 AQI Prediction":
     show_prediction()
 
