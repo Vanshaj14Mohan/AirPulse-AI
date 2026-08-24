f45d81f3ec3ef404cb027f3d74818a18a3990cef
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-08-24 23:33:24 +0530

    Files

--- Files/dashboard/pages/classification.py
+++ Files/dashboard/pages/classification.py
@@ -1,3 +1,25 @@
+# ==========================================================
+# 🌍 AirPulse AI
+# AQI Classification Page
+# ==========================================================
+
+# ===========================
+# Import Libraries
+# ===========================
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from utils import (
+    load_original_dataset,
+    load_classifier,
+    load_country_encoder,
+    load_city_encoder,
+    load_scaler
+)
+
+
 import streamlit as st
 
 def show_classification():
