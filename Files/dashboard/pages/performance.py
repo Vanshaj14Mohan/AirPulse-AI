007595e7495bf97a68549d6b1cca80c53813b4b1
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-08-26 23:23:10 +0530

    Files

--- Files/dashboard/pages/performance.py
+++ Files/dashboard/pages/performance.py
@@ -1,4 +1,26 @@
+# ==========================================================
+# 🌍 AirPulse AI
+# Model Performance Page
+# ==========================================================
+
+# ===========================
+# Import Libraries
+# ===========================
+
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+from utils import (
+    load_original_dataset,
+    load_regressor,
+    load_classifier
+)
+
+
+# ==========================================================
+# MODEL PERFORMANCE PAGE
+# ==========================================================
 
 def show_performance():
 
